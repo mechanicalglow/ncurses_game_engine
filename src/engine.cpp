92cf5ddaae88af4f3ed9bbe595f52badf69f468d
mechanicalglow <busodyes@gmail.com>
2025-12-16 13:30:21 -0600

    Added Vector 3 as a class, 3d implementations are still not correct.

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,32 +1,65 @@
+// make vec3 a class, clean up. use other formula to calculate z axis using projection
 #include <ncurses/ncurses.h>
+#include <iostream>
+#include <cmath>
+#include "vec3.h"
 
-typedef struct Vec2
-{
-    int x;
-    int y;
-} Vec2;
+const int ANGLE = 12;
+const int Z_LENGTH = 48;
 
-class Renderer
+float Vec3::half_tan(int angle)
 {
-    public:
-        Renderer()
-        {
-            
-        }
+  float half = angle / 2.0f;
+  float htan = tan(half);
+
+  return htan;
+}
+
+float Vec3::get_temp(int coord, float htan)
+{
+    if (z == 0)
+    {
+      return static_cast<float>(coord);
+    }
+
+    float temp = (coord / (z * htan));
     
-    
-};
+    if (temp > 0)
+    {
+      temp += 0.5f;
+    }
+    else 
+    {
+      temp -= 0.5f;
+    }
+
+    return temp;
+}
+
+void Vec3::xz(int angle)
+{
+    float htan = Vec3::half_tan(angle);
+    float temp = Vec3::get_temp(x, htan);
+
+    x = static_cast<int>(temp);
+}
+
+void Vec3::yz(int angle)
+{
+    float htan = Vec3::half_tan(angle);
+    float temp = Vec3::get_temp(y, htan);
+
+    y = static_cast<int>(temp);
+}
 
 class Engine
 {
     public:
-        Vec2 screen_dim;
-        Vec2 current_pos;
-        
-        Engine()
+        Vec3 screen_dim;
+        Vec3 current_pos;
+
+        Engine() : screen_dim(0,0,0), current_pos(0,0,0)
         {   
-            int x;
-            int y;
             
             initscr();
             noecho();
@@ -34,35 +67,35 @@ class Engine
             keypad(stdscr, TRUE);
             
             refresh();
-            
-            getmaxyx(stdscr, y, x);
-            
-            this->screen_dim.y = y;
-            this->screen_dim.x = x;
-            
+
+            screen_dim.x = getmaxx(stdscr);
+            screen_dim.y = getmaxy(stdscr);
+            screen_dim.z = Z_LENGTH;
+
             this->printsw();
             
             this->center_cursor();
-            this->plot_triangle();
+            this->plot_pyramid();
             
             this->enable_move();
         }
         
         void center_cursor()
         {
-            int midy = this->screen_dim.y / 2.0f;
-            int midx = this->screen_dim.x / 2.0f;
+            int midy = screen_dim.y / 2.0f;
+            int midx = screen_dim.x / 2.0f;
             
-            this->current_pos.y = midy;
-            this->current_pos.x = midx;
-            
+            current_pos.y = midy;
+            current_pos.x = midx;
+            current_pos.z = 0;
+
             move(midy, midx);
         }
         
         void move_cursor(int y, int x)
         {
-            this->current_pos.y -= y;
-            this->current_pos.x += x;
+            current_pos.y -= y;
+            current_pos.x += x;
             
             move(current_pos.y, current_pos.x);
             
@@ -71,40 +104,74 @@ class Engine
         
         void mvcursor_draw(int y, int x, char ch)
         {
-            this->move_cursor(y,x);
+            move_cursor(y,x);
             
             addch(ch);
             
             refresh();
         }
         
-        void plot_triangle()
+        void plot_pyramid()
         {
-            this->plot_point(0,0);
-            this->plot_point(0,5);
-            this->plot_point(2,3);
+            int midx = screen_dim.x / 2.0f;
+            int midy = screen_dim.y / 2.0f;
+
+            Vec3 point1(midx, midy, 0);
+            Vec3 point2(midx + 5, midy, 0);
+            Vec3 point3(midx, midy, 5);
+            Vec3 point4(midx + 5, midy, 5);
+            Vec3 point5(midx, midy + 5, 0);
+            Vec3 point6(midx, midy + 5, 5);
+            Vec3 point7(midx + 5, midy + 5, 0);
+            Vec3 point8(midx + 5, midy + 5, 5);
+
+            plot_vec3(point1);
+            plot_vec3(point2);
+            plot_vec3(point3);
+            plot_vec3(point4);
+            plot_vec3(point5);
+            plot_vec3(point6);
+            plot_vec3(point7);
+            plot_vec3(point8);
         }
         
         void plot_point(int y, int x)
         {
-            this->mvcursor_draw(y,x,'.');
-            this->center_cursor();
+            mvaddch(y, x, '.');
+
+            center_cursor();
         }
         
+        void plot_vec3(Vec3 vec)
+        {
+          static int count = 1;
+
+          std::cout << "Point " << count << " Before translation" << " X: " << vec.x << " Y: " << vec.y << std::endl;
+
+          vec.xz(ANGLE);
+          vec.yz(ANGLE);
+
+          std::cout << "Point " << count << " X: " << vec.x << " Y: " << vec.y << std::endl;
+
+          count++;
+
+          plot_point(vec.y, vec.x);
+        }
+
         void draw_line(int rs, int rn, int len, char ch)
         {
             for (int i = 0; i < len; i++)
             {
-                this->mvcursor_draw(rs, rn, ch);
+                mvcursor_draw(rs, rn, ch);
             }
         }
 
         void draw_triangle()
         {
-            this->draw_line(1, 1, 5, '/');
-            this->move_cursor(1, 0);
-            this->draw_line(-1, 1, 5, '\\');
-            this->draw_line(0,-1, 8, '_');
+            draw_line(1, 1, 5, '/');
+            move_cursor(1, 0);
+            draw_line(-1, 1, 5, '\\');
+            draw_line(0,-1, 8, '_');
         }
         
         void enable_move()
@@ -162,14 +229,14 @@ class Engine
                 refresh();
             }
             
-            printw("Y: %d, X: %d", this->current_pos.y, this->current_pos.x);
+            printw("Y: %d, X: %d, Z: %d", this->current_pos.y, this->current_pos.x, this->current_pos.z);
             
             this->move_cursor(0,0);
         }
         
         void printsw()
         {
-            printw("Screen Y: %d, Screen X: %d", this->screen_dim.y, this->screen_dim.x);
+            printw("Screen Y: %d, Screen X: %d, Screen Z: %d", this->screen_dim.y, this->screen_dim.x, this->screen_dim.z);
         }
         
         void deinit()
